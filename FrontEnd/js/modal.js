dece5b349d0d6ae29175fd4a02a6490fb281e469
nadia3190 <nadia.tlb.90@gmail.com>
2023-05-02 12:56:43 +0200

    adding modal when the user click on update button

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -7,3 +7,22 @@ if (localStorage.getItem("token")) {
     const div = document.getElementById("updates");
     div.style.display = "none";
 }
+const modal = document.getElementById("modal");
+// lorsqu'on click sur modifier on affiche une modal
+const update = document.getElementById("updates");
+const close = document.getElementById("close");
+update.addEventListener("click", function() {
+
+    modal.style.display = "block";
+});
+// lorsqu'on click sur le bouton close on ferme la modal
+close.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+// Quand l'utilisateur clique en dehors du modal, ferme le modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
